9939d9d68a5346f56b946212f1db61192c4d67c4
keaneateiq <125298107+keaneateiq@users.noreply.github.com>
2024-04-24 13:09:54 -0400

    feat: add wandb callback [WC-1253] (#4)
    * add wandb callback
    
    * chore: bumped package version
    
    * add visualization
    
    
    ---------
    
    Co-authored-by: eiq-git-autopilot <git-autopilot@evolutioniq.com>

--- pypsps/eiq/models.py
+++ pypsps/eiq/models.py
@@ -1,45 +1,52 @@
 """Model class for pypsps that is compatible and can be consumed by EIQ model wrapper."""
 
-from typing import List
 from abc import ABCMeta
+from typing import Union
+
 import numpy as np
-import tensorflow as tf
 import pandas as pd
 import pypress
 import pypress.keras.layers
 import pypress.keras.regularizers
-from pypsps.keras import losses, metrics, models
+import tensorflow as tf
+from wandb.integration import keras as wandb_keras
+
 from pypsps import inference
-from typing import Union
+from pypsps.keras import losses, metrics, models
+
+from . import utils as eiq_utils
 
 tfk = tf.keras
 
+
 class BaseLearner(metaclass=ABCMeta):
     """Base class for causal learners."""
-    @classmethod
-    def fit(self, X, treatment, y):
-        pass
 
-    @classmethod
+    def fit(self, X, treatment, y, **kwargs):
+        """Fit the model."""
+
     def predict(self, X, treatment):
-        pass
+        """Predict."""
+
 
 class PSPSLearner(BaseLearner):
     """PSPS Learner class."""
+
+    # pylint: disable=R0902
     def __init__(
         self,
         n_states: int,
         n_features: int,
         alpha: float = 1.0,
-        epochs: int = 50,
+        epochs: int = 2,
         validation_split: float = 0.2,
         batch_size: int = 64,
         learning_rate: float = 0.001,
         outcome_loss_weight: float = 0.01,
-        df_regularizer_l1: float = 10.0
+        df_regularizer_l1: float = 10.0,
     ):
         """Initialize the PSPS Learner.
-        
+
         Args:
             n_states: Number of predictive states
             n_features: Number of features
@@ -60,48 +67,70 @@ class PSPSLearner(BaseLearner):
         self.learning_rate = learning_rate
         self.outcome_loss_weight = outcome_loss_weight
         self.df_regularizer_l1 = df_regularizer_l1
-        
+
         self.model = models.build_toy_model(
             n_states=self.n_states, n_features=self.n_features, alpha=self.alpha
         )
-        
+
     def fit(self, X: pd.DataFrame, treatment: pd.Series, y: pd.Series, **kwargs):
         """Fit the model.
-        
+
         Args:
             X: Feature DataFrame
             treatment: Treatment Series, should be binary
             y: Outcome Series
         """
-       # prepare inputs and outputs
-        input_data = [np.array(X.values).astype("float32"), treatment.values]
-        output_data = np.vstack([y.values, treatment.values]).T
+        # prepare inputs and outputs
+        input_data, output_data = eiq_utils.prepare_data(X, treatment, y)
 
         # build loss function
-        psps_outcome_loss = losses.OutcomeLoss(loss=losses.NegloglikNormal(reduction="none"), reduction="auto")
-        psps_treat_loss = losses.TreatmentLoss(loss=tf.keras.losses.BinaryCrossentropy(reduction="none"), reduction="auto")
-        psps_causal_loss = losses.CausalLoss(outcome_loss=psps_outcome_loss,
-                                  treatment_loss=psps_treat_loss,
-                                  alpha=self.alpha,
-                                  outcome_loss_weight=self.outcome_loss_weight,
-                                  predictive_states_regularizer=pypress.keras.regularizers.DegreesOfFreedom(l1 = self.df_regularizer_l1, df= self.n_states - 1),
-                                  reduction="auto")
+        psps_outcome_loss = losses.OutcomeLoss(
+            loss=losses.NegloglikNormal(reduction="none"), reduction="auto"
+        )
+        psps_treat_loss = losses.TreatmentLoss(
+            loss=tf.keras.losses.BinaryCrossentropy(reduction="none"), reduction="auto"
+        )
+        psps_causal_loss = losses.CausalLoss(
+            outcome_loss=psps_outcome_loss,
+            treatment_loss=psps_treat_loss,
+            alpha=self.alpha,
+            outcome_loss_weight=self.outcome_loss_weight,
+            predictive_states_regularizer=pypress.keras.regularizers.DegreesOfFreedom(
+                l1=self.df_regularizer_l1, df=self.n_states - 1
+            ),
+            reduction="auto",
+        )
 
         # compile model
-        self.model.compile(loss=psps_causal_loss,
-              optimizer=tfk.optimizers.Nadam(learning_rate=self.learning_rate),
-              metrics=[metrics.propensity_score_crossentropy])
+        self.model.compile(
+            loss=psps_causal_loss,
+            optimizer=tfk.optimizers.Nadam(learning_rate=self.learning_rate),
+            metrics=[metrics.propensity_score_crossentropy],
+        )
 
         # fit
-        history = self.model.fit(input_data, output_data, epochs=self.epochs, batch_size=self.batch_size,
-                    validation_split=self.validation_split,
-                    callbacks=models.recommended_callbacks(), **kwargs)
-        
+        self.model.fit(
+            input_data,
+            output_data,
+            epochs=self.epochs,
+            batch_size=self.batch_size,
+            validation_split=self.validation_split,
+            callbacks=models.recommended_callbacks() + [wandb_keras.WandbCallback()],
+            **kwargs,
+        )
+
+        # visualize results
+        eiq_utils.visualize_results(self.model, X, treatment, y)
+
         return self
 
-    def predict(self, X: pd.DataFrame, treatment: pd.Series) -> Union[pd.DataFrame, np.ndarray]:
+    def predict(
+        self,
+        X: Union[pd.DataFrame, np.ndarray],
+        treatment: Union[pd.DataFrame, np.ndarray],
+    ) -> Union[pd.DataFrame, np.ndarray]:
         """Predict treatment effect.
-        
+
         Args:
             X: Feature Dataframe
             treatment: Treatment Series, should be binary
@@ -109,5 +138,5 @@ class PSPSLearner(BaseLearner):
         Return:
             Outcome and propensity score for each row in X
         """
-        X = np.array(X.values).astype("float32")
+        (X, treatment), _ = eiq_utils.prepare_data(X, treatment)
         return inference.predict_outcome_propensity_ute(self.model, X, treatment)
---
+++ pypsps/eiq/utils.py
@@ -0,0 +1,162 @@
+"""Util functions for EIQ-specific implementation."""
+from typing import Union
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sklearn.metrics
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+import wandb
+from pypsps import inference, utils
+
+tfk = tf.keras
+
+
+def prepare_data(
+    X: Union[pd.DataFrame, np.ndarray],
+    treatment: Union[pd.Series, np.ndarray],
+    y: Union[pd.Series, np.ndarray] = None,
+) -> tuple:
+    """Prepare data for training and prediction.
+
+    Args:
+        X: Feature Dataframe or Array
+        treatment: Treatment Series or Array, should be binary
+        y: Outcome Series or Array
+
+    Return:
+        input_data: List of input and treatment
+        output_data: Array of stacked output and treatment
+    """
+    if (
+        isinstance(X, np.ndarray)
+        and isinstance(treatment, np.ndarray)
+        and isinstance(y, np.ndarray)
+    ):
+        input_data = [X, treatment]
+        output_data = np.hstack([y, treatment]) if y is not None else None
+    else:
+        input_data = [np.array(X.values).astype("float32"), treatment.values]
+        output_data = (
+            np.vstack([y.values, treatment.values]).T if y is not None else None
+        )
+    return input_data, output_data
+
+
+def _eval_propensity(y_true, y_score):
+    """Evaluate propensity score and visualize results.
+
+    Args:
+        y_true: True treatment values
+        y_score: Predicted propensity scores
+    """
+    # TODO: Replace with curve estimator from ml-foundation
+    y_comb = pd.DataFrame({"treatment": y_true, "propensity_score": y_score})
+    plt.subplots(figsize=(10, 4))
+    sns.displot(data=y_comb, x="propensity_score", hue="treatment")
+    wandb.log(
+        {
+            "propensity_score_vs_treatment": wandb.Image(
+                plt, caption="Propensity Score vs Treatment"
+            )
+        }
+    )
+
+    precision, recall, _ = sklearn.metrics.precision_recall_curve(y_true, y_score)
+    sklearn.metrics.PrecisionRecallDisplay(precision=precision, recall=recall).plot()
+    auc_precision_recall = sklearn.metrics.auc(recall, precision)
+
+    plt.title(f"Precision Recall Curve (AUC: {auc_precision_recall:.2f})")
+    plt.grid()
+    wandb.log(
+        {"precision_recall_curve": wandb.Image(plt, caption="Precision Recall Curve")}
+    )
+
+
+def visualize_results(
+    model: tf.keras.Model,
+    X: Union[pd.DataFrame, np.ndarray],
+    treatment: Union[pd.Series, np.ndarray],
+    y: Union[pd.Series, np.ndarray],
+):
+    """Visualize the results."""
+    # prepare inputs and outputs
+    input_data, output_data = prepare_data(X, treatment, y)
+
+    # inference
+    y_pred = model.predict(input_data)
+    outcome_pred, _, propensity_score, weights = utils.split_y_pred(y_pred)
+
+    # evaluate propensity and show precision-call curve
+    _eval_propensity(output_data[:, 1], propensity_score.ravel())
+
+    # get the weights of all states
+    weights_df = pd.DataFrame(
+        weights, columns=["state" + str(i) for i in range(weights.shape[1])]
+    )
+
+    # show total weights of the states
+    plt.subplots(figsize=(10, 4))
+    # TODO: add a mean(weights_df.iloc[:, i] * treatment) for each i (barchart)
+    weights_df.sum().plot.barh()
+    wandb.log({"weights": wandb.Image(plt, caption="weights")})
+
+    # show mean weights of the states for treatment = 1
+    plt.subplots(figsize=(10, 4))
+    np.mean(weights_df * treatment).plot.barh()
+    wandb.log(
+        {
+            "mean state weight for treatment = 1": wandb.Image(
+                plt, caption="mean state weight for treatment = 1"
+            )
+        }
+    )
+
+    # show heatmap of the states
+    plt.subplots(figsize=(10, 4))
+    np.random.seed(42)  # sample 1000 units
+    weights_df = weights_df.iloc[
+        sorted(np.random.choice(range(weights_df.shape[0]), 500, replace=False)), :
+    ]
+    sns.heatmap(weights_df.round(2))
+    wandb.log({"heatmap": wandb.Image(plt, caption="heatmap")})
+
+    # show pairplot
+    y_df = pd.DataFrame(
+        outcome_pred,
+        columns=["y_pred" + str(i) for i in range(outcome_pred.shape[1])],
+    )
+    y_df["outcome"] = output_data[:, 0]
+    plt.subplots(figsize=(10, 4))
+    sns.pairplot(y_df)
+    wandb.log({"pairplot": wandb.Image(plt, caption="pairplot")})
+
+    # show ute distribution
+    ute = inference.predict_ute(model, input_data[0])
+    _, axe = plt.subplots(figsize=(10, 4))
+    sns.displot(ute, ax=axe)
+    plt.axvline(
+        x=output_data[output_data[:, 1] == 1, 0].mean()
+        - output_data[output_data[:, 1] == 0, 0].mean(),
+        ymax=axe.get_ylim()[1],
+        color="red",
+        label="naive ate",
+    )
+    plt.axvline(x=np.mean(ute), ymax=axe.get_ylim()[1], color="blue", label="ate")
+    plt.legend()
+    wandb.log({"ute": wandb.Image(plt, caption="ute")})
+
+    # show ute distribution without naive ate
+    _, axe = plt.subplots(figsize=(10, 4))
+    sns.displot(ute, ax=axe)
+    plt.axvline(x=np.mean(ute), ymax=axe.get_ylim()[1], color="blue", label="ate")
+    plt.legend()
+    wandb.log({"ute distribution without naive ate": wandb.Image(plt, caption="ute")})
+
+    # show cumulative ute distribution
+    plt.subplots(figsize=(10, 4))
+    sns.ecdfplot(ute)
+    plt.grid()
+    wandb.log({"ute_cumulative": wandb.Image(plt, caption="ute_cumulative")})
